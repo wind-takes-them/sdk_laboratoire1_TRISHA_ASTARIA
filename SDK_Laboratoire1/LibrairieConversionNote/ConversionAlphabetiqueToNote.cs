bbd58aa46cdfb5fe7b9cbf09d3d294726b3b68f8
wind-takes-them <134608206+wind-takes-them@users.noreply.github.com>
2023-06-12 19:32:38 -0400

    Libraire de conversion

---
+++ SDK_Laboratoire1/LibrairieConversionNote/ConversionAlphabetiqueToNote.cs
@@ -0,0 +1,40 @@
+﻿namespace LibrairieConversionNote
+{
+    public class ConversionAlphabetiqueToNote
+    {
+        public string Transformer (string NoteAlphabetique)
+        {
+            var returnStr = "Entree Invalide";
+
+            switch (NoteAlphabetique)
+            {
+                case "A":
+                    returnStr = "LA";
+                    break;
+                case "B":
+                    returnStr = "SI";
+                    break;
+                case "C":
+                    returnStr = "DO";
+                    break;
+                case "D":
+                    returnStr = "RE";
+                    break;
+                case "E":
+                    returnStr = "MI";
+                    break;
+                case "F":
+                    returnStr = "FA";
+                    break;
+                case "G":
+                    returnStr = "SOL";
+                    break;
+            }
+            
+            return returnStr;
+        }
+
+
+
+    }
+}
